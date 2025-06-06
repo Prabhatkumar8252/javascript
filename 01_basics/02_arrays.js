fb98ab1ffedaaf5c5ceaf8fed4e298c1f1971f0c
Prabhatkumar8252 <121727522+Prabhatkumar8252@users.noreply.github.com>
2025-06-06 13:16:34 +0530

    arrays

--- 01_basics/02_arrays.js
+++ 01_basics/02_arrays.js
@@ -4,37 +4,37 @@
 
 const myArray = [2, 4, 5, 7, 8]
 const myHeros = ["shaktiman", "naagraj", "doga"]
+
 const myArray2 = new Array(3, 6, 7, 8, 9)
 console.log(myArray[0]);
 console.log(myArray2[1]);
 console.log(myHeros[2]);
 
 // Array Methods
-
-myArray.push(7)
-myArray.push(6)
+myArray.push(9)
+myArray.push(3)
 myArray.pop()
 myArray.unshift(1)
+
+console.log(myArray);
+
 myArray.shift()
-
-console.log(myArray.includes(9));
-console.log(myArray.indexOf(9));
 console.log(myArray);
 
-const newArr = myArray.join()
-console.log(myArray);
-console.log( typeof newArr); // string
+console.log(myArray.includes(10));
+console.log(myArray.indexOf(5));
 
-// slice, splice
+const newArray = myArray.join()
+console.log(newArray);
+console.log( typeof newArray);
 
+// slice splice
 console.log("A ", myArray);
 
-const myn1 = myArray.slice(1, 3)
+const myn1 = myArray.slice(2, 4)
 console.log(myn1);
 console.log("B ", myArray);
 
 
 const myn2 = myArray.splice(1, 5)
 console.log(myn2);
-
-
